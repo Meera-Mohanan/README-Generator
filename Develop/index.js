86c79e10b9ce4dd3adcefe32d442a51053c9ecaa
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-05-21 23:17:16 +0800

    .gitignore updated

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,6 @@ const path = require("path")
 const fs = require("fs")
 const inquirer = require("inquirer")
 const generateMarkdown = require("./utils/generateMarkdown") //imports generatemakedown 
-
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -57,13 +56,11 @@ const questions = [
 
 ];
 
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data)
     // use fs to sync, path after to join to current working directory then add file name and data //
 }
-
 
 // TODO: Create a function to initialize app
 function init() {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,38 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license !== "None") {
-    return `![License](https://img.shields.io/badge/License-${license}%20-blue.svg)`
-  }
-
-}
+function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  return `![License Link](https://opensource.org/licenses/${license}-.org/)`
+function renderLicenseLink(license) {}
 
-}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  return `### License - ${license}
-  
-  `;
-}
+function renderLicenseSection(license) {}
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  ${renderLicenseBadge(data.license)}
-  ${renderLicenseSection(data.license)}
-  ${renderLicenseLink(data.license)}
-  # Project Title -  ${data.title} 
-   ## Description - \n* ${data.description} \n
-   ## Installation - \n* ${data.install} \n
-   ## Contributing - \n* ${data.contributing} \n
-   ## Tests - \n* ${data.tests} \n
-   ## Live Link - \n* ${data.url} \n
-   ## Questions - \n* Check out my GitHub - ${data.github} \n* Email me - ${data.email} \n
+  return `# ${data.title}
+
 `;
 }
+
 module.exports = generateMarkdown;
